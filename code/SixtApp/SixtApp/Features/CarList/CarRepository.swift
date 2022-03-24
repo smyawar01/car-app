f802fc7cf892ff07bae71403f30a8a0639b53b72
Yawar Muhammad <smyawar01@gmail.com>
2022-03-24 18:18:43 +0500

    - Create CarRepository

---
+++ code/SixtApp/SixtApp/Features/CarList/CarRepository.swift
@@ -0,0 +1,22 @@
+//
+//  CarsRepository.swift
+//  SixtApp
+//
+//  Created by Yawar Ali on 23/03/2022.
+//
+
+import Foundation
+
+protocol CarRepository {
+    
+    func getAll(completion: @escaping ((Result<[Car], Error>) -> Void))
+}
+struct CarRepositoryImpl: CarRepository {
+    
+    let carRepositoryRemote: CarRepositoryRemote
+    
+    func getAll(completion: @escaping ((Result<[Car], Error>) -> Void)) {
+        
+        carRepositoryRemote.getAll(completion: completion)
+    }
+}
