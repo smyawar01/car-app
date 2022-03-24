b2a5a900e37c18c0b25429406ab408cdd4b6c2d9
Yawar Muhammad <smyawar01@gmail.com>
2022-03-24 18:20:48 +0500

    - Implement Car Fetch Use case

---
+++ code/SixtApp/SixtApp/Features/CarList/FetchCarsUseCase.swift
@@ -0,0 +1,43 @@
+//
+//  FetchCarsUseCase.swift
+//  SixtApp
+//
+//  Created by Yawar Ali on 23/03/2022.
+//
+
+import Foundation
+
+protocol FetchCarsUseCase: AnyObject {
+    
+    typealias Response = ((Result<([Car], [CarViewData]), Error>) -> Void)
+    var onExecuted: Response? { get set }
+    func execute()
+}
+
+class FetchCarsUseCaseImpl: FetchCarsUseCase {
+    
+    let carRepository: CarRepository
+    let carMapper: CarMapper
+    var onExecuted: Response?
+    
+    init(carRepository: CarRepository, carMapper: CarMapper) {
+        
+        self.carRepository = carRepository
+        self.carMapper = carMapper
+    }
+    
+    func execute() {
+        
+        carRepository.getAll { [weak self] result in
+            
+            guard let self = self else { return }
+            switch result {
+                
+            case .success(let cars):
+                self.onExecuted?(.success((cars, self.carMapper.map(cars: cars))))
+            case .failure(let error):
+                self.onExecuted?(.failure(error))
+            }
+        }
+    }
+}
