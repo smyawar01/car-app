25a85feb54dd348236b1796d356185a3fa2da0c6
Yawar Muhammad <smyawar01@gmail.com>
2022-03-24 18:19:43 +0500

    - Create AppFactory

---
+++ code/SixtApp/SixtApp/Main/AppFactory.swift
@@ -0,0 +1,28 @@
+//
+//  AppFactory.swift
+//  SixtApp
+//
+//  Created by Yawar Ali on 24/03/2022.
+//
+
+import Foundation
+
+struct AppFactory {
+    
+    let networkService: NetworkService
+    init() {
+        
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .useDefaultKeys
+        networkService = NetworkSerivceImpl(session: URLSession.shared,
+                                            decoder: decoder)
+    }
+}
+
+extension AppFactory {
+    
+    func makeCarListFactory() -> CarListFactory {
+        
+        return .init(networkService: networkService)
+    }
+}
