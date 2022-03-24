825588d578cea2e80a4d07a5c47e5a3285d6a435
Yawar Muhammad <smyawar01@gmail.com>
2022-03-24 18:18:01 +0500

    - Create CarViewData

---
+++ code/SixtApp/SixtApp/Features/CarList/CarViewData.swift
@@ -0,0 +1,19 @@
+//
+//  CarViewData.swift
+//  SixtApp
+//
+//  Created by Yawar Ali on 23/03/2022.
+//
+
+import Foundation
+
+struct CarViewData {
+    
+    let identifier: String
+    let modelName: String
+    let name: String
+    let make: String
+    let licensePlate: String
+    let innerCleanliness: String
+    let carImageUrl: String
+}
