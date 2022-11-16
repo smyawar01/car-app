45da3c5f4f3677a55d0f5936fd01bb858e4e6905
Bassam <bassam.fouad@10pearls.com>
2022-11-17 02:47:55 +0500

    network module integrated from remote

--- code/SixtApp/SixtApp/Features/CarMap/CarMapViewModel.swift
+++ code/SixtApp/SixtApp/Features/CarMap/CarMapViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import NetworkModule
 
 protocol CarMapViewModel: AnyObject {
     
--- code/SixtApp/SixtApp/Main/AppFactory.swift
+++ code/SixtApp/SixtApp/Main/AppFactory.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import NetworkModule
 
 struct AppFactory {
     
